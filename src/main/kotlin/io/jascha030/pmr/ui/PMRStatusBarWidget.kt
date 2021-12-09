1ddcf44b85f9e3e9f28c88b1ff5a25419979a598
Jascha030 <contact@jaschavanaalst.nl>
2021-12-10 00:16:48 +0100

    ✨ Create private widget implementation

--- src/main/kotlin/io/jascha030/pmr/ui/PMRStatusBarWidget.kt
+++ src/main/kotlin/io/jascha030/pmr/ui/PMRStatusBarWidget.kt
@@ -6,20 +6,33 @@ import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.StatusBarWidgetFactory
 
-private const val pmrWidgetId: String = "pmrWidget"
+private const val WIDGET_ID: String = "pmrWidget"
 
 class PMRStatusBarWidgetFactory : StatusBarWidgetFactory {
-    override fun getId(): String = pmrWidgetId
+    override fun getId(): String = WIDGET_ID
 
     override fun getDisplayName(): String = "PMR"
 
     override fun isAvailable(project: Project): Boolean = true
 
-    override fun createWidget(project: Project): StatusBarWidget {
-        TODO("Not yet implemented")
-    }
+    override fun createWidget(project: Project): StatusBarWidget = PMRStatusBarWidget(project)
 
     override fun disposeWidget(widget: StatusBarWidget) = Disposer.dispose(widget)
 
     override fun canBeEnabledOn(statusBar: StatusBar): Boolean = true
+}
+
+private class PMRStatusBarWidget(private val project: Project) : StatusBarWidget {
+    private var statusBar: StatusBar? = null
+
+    override fun ID(): String = WIDGET_ID
+
+    override fun dispose() {
+        this.statusBar = null
+    }
+
+    override fun install(statusBar: StatusBar) {
+        this.statusBar = statusBar;
+        statusBar.updateWidget(ID())
+    }
 }
