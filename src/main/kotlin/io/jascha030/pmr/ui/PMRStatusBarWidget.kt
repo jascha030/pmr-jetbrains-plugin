389ed14ae3fb3945bad41d7831e011cee7c14f9c
Jascha030 <contact@jaschavanaalst.nl>
2021-12-09 23:52:59 +0100

    🎨 Simplify methods where possible

--- src/main/kotlin/io/jascha030/pmr/ui/PMRStatusBarWidget.kt
+++ src/main/kotlin/io/jascha030/pmr/ui/PMRStatusBarWidget.kt
@@ -1,32 +1,25 @@
 package io.jascha030.pmr.ui
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.StatusBarWidgetFactory
 
 private const val pmrWidgetId: String = "pmrWidget"
 
-class PMRStatusBarWidgetFactory: StatusBarWidgetFactory {
+class PMRStatusBarWidgetFactory : StatusBarWidgetFactory {
     override fun getId(): String = pmrWidgetId
 
-    override fun getDisplayName(): String {
-        return "PMR"
-    }
+    override fun getDisplayName(): String = "PMR"
 
-    override fun isAvailable(project: Project): Boolean {
-        return true
-    }
+    override fun isAvailable(project: Project): Boolean = true
 
     override fun createWidget(project: Project): StatusBarWidget {
         TODO("Not yet implemented")
     }
 
-    override fun disposeWidget(widget: StatusBarWidget) {
-        TODO("Not yet implemented")
-    }
+    override fun disposeWidget(widget: StatusBarWidget) = Disposer.dispose(widget)
 
-    override fun canBeEnabledOn(statusBar: StatusBar): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun canBeEnabledOn(statusBar: StatusBar): Boolean = true
 }
