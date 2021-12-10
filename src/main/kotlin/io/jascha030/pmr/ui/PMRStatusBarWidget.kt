c5b8ecc32e5bf4562a235c2f930c87e3569e437c
Jascha030 <contact@jaschavanaalst.nl>
2021-12-10 09:18:29 +0100

    ✨ Add MultipleTextValuesPresentation interface to PMRStatusBarWidget

--- src/main/kotlin/io/jascha030/pmr/ui/PMRStatusBarWidget.kt
+++ src/main/kotlin/io/jascha030/pmr/ui/PMRStatusBarWidget.kt
@@ -1,10 +1,13 @@
 package io.jascha030.pmr.ui
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.popup.ListPopup
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.StatusBarWidgetFactory
+import com.intellij.util.Consumer
+import java.awt.event.MouseEvent
 
 private const val WIDGET_ID: String = "pmrWidget"
 
@@ -22,17 +25,32 @@ class PMRStatusBarWidgetFactory : StatusBarWidgetFactory {
     override fun canBeEnabledOn(statusBar: StatusBar): Boolean = true
 }
 
-private class PMRStatusBarWidget(private val project: Project) : StatusBarWidget {
+private class PMRStatusBarWidget(private val project: Project) : StatusBarWidget, StatusBarWidget.MultipleTextValuesPresentation {
     private var statusBar: StatusBar? = null
+    private var selectedValue: String? = null
 
     override fun ID(): String = WIDGET_ID
 
     override fun dispose() {
-        this.statusBar = null
+        statusBar = null
     }
 
     override fun install(statusBar: StatusBar) {
         this.statusBar = statusBar;
         statusBar.updateWidget(ID())
     }
+
+    override fun getPresentation(): StatusBarWidget.WidgetPresentation = this
+
+    override fun getTooltipText(): String = "Quick access to resources"
+
+    override fun getClickConsumer(): Consumer<MouseEvent>? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPopupStep(): ListPopup? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSelectedValue(): String? = this.selectedValue
 }
