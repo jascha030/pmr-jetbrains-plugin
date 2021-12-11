e8748285e91b4cc70f2d7fe841b3d1693275ce47
Jascha030 <contact@jaschavanaalst.nl>
2021-12-11 21:48:38 +0100

    ✨ Add ResourceActionGroup implementation

---
+++ src/main/kotlin/io/jascha030/pmr/action/ResourceActionGroup.kt
@@ -0,0 +1,11 @@
+package io.jascha030.pmr.action
+
+import com.intellij.openapi.actionSystem.ActionGroup
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class ResourceActionGroup : ActionGroup() {
+    override fun getChildren(e: AnActionEvent?): Array<AnAction> {
+        TODO("Not yet implemented")
+    }
+}
