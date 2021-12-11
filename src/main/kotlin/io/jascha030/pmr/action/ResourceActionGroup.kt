de65a76b7a8593ca15f68fe37895d29d8cb153c7
Jascha030 <contact@jaschavanaalst.nl>
2021-12-12 00:37:58 +0100

    Add test resource

--- src/main/kotlin/io/jascha030/pmr/action/ResourceActionGroup.kt
+++ src/main/kotlin/io/jascha030/pmr/action/ResourceActionGroup.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
 class ResourceActionGroup : ActionGroup() {
-    override fun getChildren(e: AnActionEvent?): Array<AnAction> {
-        TODO("Not yet implemented")
-    }
+    override fun getChildren(e: AnActionEvent?): Array<GoToResourceAction> = arrayOf(
+        GoToResourceAction("https://google.nl", "Google", "Test", null)
+    )
 }
