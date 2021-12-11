9d61c5e1562a70217e99b76ce456f6d957d3d9ea
Jascha030 <contact@jaschavanaalst.nl>
2021-12-12 00:36:41 +0100

    ✨ Add asbstract resource action classes

--- src/main/kotlin/io/jascha030/pmr/action/GoToResourceAction.kt
+++ src/main/kotlin/io/jascha030/pmr/action/GoToResourceAction.kt
@@ -1,21 +1,17 @@
 package io.jascha030.pmr.action
 
-import com.intellij.ide.BrowserUtil
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.util.NlsActions
+import javax.swing.Icon
 
-abstract class GoToResourceAction : AnAction() {
-    abstract fun getResourceURI(): String?
-
-    override fun actionPerformed(e: AnActionEvent) {
-        getResourceURI()?.let {
-            BrowserUtil.browse(it)
-        }
-    }
-
-    override fun update(e: AnActionEvent) {
-        getResourceURI().let {
-            e.presentation.isEnabled = (it != null)
-        }
-    }
+class GoToResourceAction(
+    private var resourceUri: String?,
+    text: @NlsActions.ActionText String?,
+    description: @NlsActions.ActionDescription String?,
+    icon: Icon?
+) : OpenInBrowserAction(
+    text,
+    description,
+    icon
+) {
+    override fun getResourceURI(): String? = this.resourceUri
 }
